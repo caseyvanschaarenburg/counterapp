ea00dc246f2195b866a3fff1198538c2fc139fbe
casey van schaarenburg <caseyschaarenburg@gmail.com>
2021-02-03 15:13:01 +0100

    dit is wel de counterapp lol :)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# counterapp" 
+1. npm install
+2. npm run start
+
+<!-- Copyright 2020 Casey van schaarenburg --> 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const NavBar = ({totalCounters})=> {
+     return ( 
+            <nav className="navbar navbar-light bg-light">
+              <a className="navbar-brand" href="#">
+              Counter with React{" "} 
+              <span className="badge badge-pill badge-secondary">
+              {totalCounters}
+              </span>
+            </a>
+        </nav>
+         );
+    };
+
+
+
+ 
+export default NavBar ;
